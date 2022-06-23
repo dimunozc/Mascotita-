31d898bdbc141fc38d9bd0b9cdd6c772340cdbfc
XFelix578 <102106351+XFelix578@users.noreply.github.com>
2022-06-23 17:31:08 -0400

    commit

--- Grupo4_FelipeyDiego-nota3/core/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from .models import Producto
-
-class ProductoForm(ModelForm):
-    class Meta:
-        model = Producto
-        fields = ['ProductoId', 'Descripcion', 'Valor', 'Stock']
