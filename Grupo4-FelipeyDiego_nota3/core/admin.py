c70771bdfb7fb3a95d86bd1477a17f4280835380
XFelix578 <102106351+XFelix578@users.noreply.github.com>
2022-06-24 20:05:20 -0400

    Delete Grupo4-FelipeyDiego_nota3 directory

--- Grupo4-FelipeyDiego_nota3/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Carrito, DetalleCarrito, Persona, Producto
-
-# Register your models here.
-admin.site.register(Persona)
-admin.site.register(Carrito)
-admin.site.register(DetalleCarrito)
-admin.site.register(Producto)
