eeb39212cb18d30372acf6ff0a9388ceed80fa07
XFelix578 <102106351+XFelix578@users.noreply.github.com>
2022-07-01 20:19:21 -0400

    commit

--- Prueba3/ECOMMERCE/settings.py
+++ Prueba3/ECOMMERCE/settings.py
@@ -80,10 +80,29 @@ WSGI_APPLICATION = 'ECOMMERCE.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/xe',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'TEST': {
+            'USER': 'default_test',
+            'TBLSPACE': 'default_test_tbls',
+            'TBLSPACE_TMP': 'default_test_tbls_tmp',
+        },
+    },
+    'other': {
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': '127.0.0.1:1521/XE',
+        'USER': 'django1',
+        'PASSWORD': 'django1',
+        'TEST': {
+            'USER': 'other_test',
+            'TBLSPACE': 'other_test_tbls',
+            'TBLSPACE_TMP': 'other_test_tbls_tmp',
+        },
+    },
 }
+
 
 
 # Password validation
